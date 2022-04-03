3040bb5db7606180e82e1c121bbdeb01998713f8
pomtcom <pomtcom@pomtcoms-MacBook-Pro.local>
2022-04-03 23:48:55 +0700

    update new signature

--- main_module.py
+++ main_module.py
@@ -11,6 +11,10 @@ eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9SSS
 VFRCX0hFTExPX1dPTFJE
 """
 
+"""
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJUVEJfSEVMTE9fV09MUkQifQ.cUbDDWAu-uXkiPvw-EJy3SwnNZjWMQGlGBmMDVh9QJU
+"""
+
 
 def main_module_library():
     # to do or write any code of main usage module
