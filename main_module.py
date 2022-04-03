e6e565ece075d62f878244edf766497748221637
pomtcom <pomtcom@pomtcoms-MacBook-Pro.local>
2022-04-03 23:26:07 +0700

    first commit

---
+++ main_module.py
@@ -0,0 +1,13 @@
+"""
+This is main module function which written by software engineer#2
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJUVEJfSEVMTE9fV09MUkQifQ.cUbDDWAu-uXkiPvw-EJy3SwnNZjWMQGlGBmMDVh9QJU
+"""
+
+
+
+def main_module_library():
+    # to do or write any code of main usage module
+    pass
+
+
+
